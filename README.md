3b3b11defe6c4b8a4202aabf6405cdc06b611b6a
mertsirvanci <merts@mertsirvancis-MacBook-Pro.local>
2021-10-23 20:55:12 +0300

    added diagram and added kubectl

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-I created EKS Cluster on console.aws.amazon.com
+First create VPC with `create-vpc-stack.sh`
+I created EKS Cluster on console.aws.amazon.comdd"
--- update-kubeconfig.sh
+++ update-kubeconfig.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-aws eks --region eu-central-1 update-kubeconfig --name capstone-eks-cluster --role-arn arn:aws:iam::718227099436:user/devmerts
+aws eks --region eu-central-1 update-kubeconfig --name capstone-eks-cluster
