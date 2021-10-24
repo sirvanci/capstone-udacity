a9727cf433b7289d010042d1d8473324800100cb
mertsirvanci <merts@mertsirvancis-MacBook-Pro.local>
2021-10-24 03:07:30 +0300

    feature blue-green deployment

--- EKS/create-eksctl-cluster.sh
+++ EKS/create-eksctl-cluster.sh
@@ -1,4 +1,4 @@
 #! bin/bash
-
+#To-do: get vpc subnet from resources 
 eksctl create cluster \
   -f cluster.yaml
--- capstone-rc-frontend/src/App.js
+++ capstone-rc-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi <code>!</code> I'm Mert Şirvancı
         </p>
         <a
           className="App-link"
