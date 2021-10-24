19d9dea42993ad326fd9d1d9aed6084f1e147130
mertsirvanci <merts@mertsirvancis-MacBook-Pro.local>
2021-10-24 06:41:46 +0300

    green background new version

--- capstone-rc-frontend/src/App.js
+++ capstone-rc-frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi <code>!</code> I'm Mert Şirvancı
+         Hi <code>!</code> I'm Mert Şirvancı - Green Deployment - New Version Ready !
         </p>
         <a
           className="App-link"
